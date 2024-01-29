9f0b3f59cf53082f79ed6abafb6d2075e46ca2cf
ricardocollado <ricardocolladorothschild@gmail.com>
2024-01-29 12:49:32 -0400

    start building Products model

--- models/Products.model.ts
+++ models/Products.model.ts
@@ -1,1 +1,28 @@
-import { DataType, Model } from ''
+import { DataTypes, Model } from 'sequelize';
+import sequelize from './sequelize.config';
+
+class Products extends Model{
+    public products_id!:number;
+    public name!:string;
+    public description!:string;
+    public price!:number;
+    public img_url!:string;
+
+
+}
+
+
+Products.init({
+    products_id:{
+        type:DataTypes.INTEGER,
+        primaryKey:true,
+     }
+   
+    },
+    {
+        sequelize,
+        tableName:'products'
+    }
+    
+    );
+
