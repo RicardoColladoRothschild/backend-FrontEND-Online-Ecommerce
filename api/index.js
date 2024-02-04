c0cb4dd8264e5bd2f40e341a2a520063c3d14835
ricardocollado <ricardocolladorothschild@gmail.com>
2024-02-04 15:58:36 -0400

    modified mannually index.js

--- api/index.js
+++ api/index.js
@@ -1,23 +1,21 @@
-"use strict";
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
-};
-Object.defineProperty(exports, "__esModule", { value: true });
-const express_1 = __importDefault(require("express"));
-const index_1 = __importDefault(require("./routes/index"));
-const app = (0, express_1.default)();
+const express = require('express');
+const app = express();
+const routerApi = require('./routes/index');
 const PORT = process.env.PORT || 3031;
-app.use(express_1.default.json());
+app.use(express.json());
+
 app.get('/api', (request, response) => {
     response.send('Esta es una ruta normal');
 });
+
 app.get('/api/rutatesting', (request, response) => {
     response.send('Hola, estamos probando rooting');
 });
-/*async function startserver() {
+
+async function startserver() {
         await sequelize.sync();
         
-    }*/
-(0, index_1.default)(app);
+    }
 app.listen(PORT);
-//startserver();
+routerApi(app);
+startserver();
