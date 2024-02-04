616b124b4cf99beef77a5744b5cd5a557f2968b0
ricardocollado <ricardocolladorothschild@gmail.com>
2024-02-04 15:44:39 -0400

    new transpilation

--- api/index.js
+++ api/index.js
@@ -4,20 +4,20 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
-const sequelize_config_1 = __importDefault(require("./models/sequelize.config"));
 const index_1 = __importDefault(require("./routes/index"));
-const server = (0, express_1.default)();
+const app = (0, express_1.default)();
 const PORT = process.env.PORT || 3031;
-server.use(express_1.default.json());
-server.get('/api', (request, response) => {
+app.use(express_1.default.json());
+app.get('/api', (request, response) => {
     response.send('Esta es una ruta normal');
 });
-server.get('/api/rutatesting', (request, response) => {
+app.get('/api/rutatesting', (request, response) => {
     response.send('Hola, estamos probando rooting');
 });
-async function startServer() {
-    await sequelize_config_1.default.sync();
-}
-server.listen(PORT);
-startServer();
-(0, index_1.default)(server);
+/*async function startserver() {
+        await sequelize.sync();
+        
+    }*/
+(0, index_1.default)(app);
+app.listen(PORT);
+//startserver();
