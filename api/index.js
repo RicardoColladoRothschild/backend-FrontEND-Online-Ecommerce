bc9e7af2e868f659e4b0f3122d509bd1ec02c3f4
ricardocollado <ricardocolladorothschild@gmail.com>
2024-02-04 16:38:57 -0400

    change users.routes.js

--- api/index.js
+++ api/index.js
@@ -1,21 +1,13 @@
 const express = require('express');
 const app = express();
 const routerApi = require('./routes/index');
-const PORT = process.env.PORT || 3031;
+const PORT = process.env.PORT || 3030;
+
 app.use(express.json());
 
-app.get('/api', (request, response) => {
-    response.send('Esta es una ruta normal');
+app.get('/api', (req,res)=>{
+    res.send('Hello, trying a seerverles routes');
 });
 
-app.get('/api/rutatesting', (request, response) => {
-    response.send('Hola, estamos probando rooting');
-});
-
-async function startserver() {
-        await sequelize.sync();
-        
-    }
 app.listen(PORT);
 routerApi(app);
-startserver();
