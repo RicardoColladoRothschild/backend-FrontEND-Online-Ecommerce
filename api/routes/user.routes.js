6a0d5b647144a40ed447e270d8b3c4a71537b787
ricardocollado <ricardocolladorothschild@gmail.com>
2024-02-04 17:19:02 -0400

    start creating

--- api/routes/user.routes.js
+++ api/routes/user.routes.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-
+const Users = require('../model/users.model');
 const allUsers = [
     {
         name:'ricardo',
@@ -12,6 +12,25 @@ const allUsers = [
 router.get('/all', (req, res)=>{
         res.status(200).json(allUsers);
 });
+
+router.get('/allusersdb', async (req, res)=>{
+
+    try{
+
+        const user = await Users.findAll();
+        
+        
+            res
+                .status(200)
+                .json({user});
+    }catch(error){
+        
+        res
+        .status(500)
+        .json({error:`Internal server error - 500: ${error}`});
+    }
+});
+
 
 
 module.exports = router;
