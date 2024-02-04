db3d6b9682842847938b9ce062398ca02dc7042b
ricardocollado <ricardocolladorothschild@gmail.com>
2024-02-04 14:35:28 -0400

    eliminate routing

--- api/index.js
+++ api/index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
-const routerApi = require('./routes/');
+
 const PORT = process.env.PORT || 3030;
 
 app.use(express.json());
@@ -9,4 +9,3 @@ app.get('/api', (req,res)=>{
     res.send('Hello, trying a seerverles routes');
 });
 
-routerApi(app);
