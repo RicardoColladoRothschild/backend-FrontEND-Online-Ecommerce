582eb29e6cd4a6298ecf7495ae887b60e5eeb28b
ricardocollado <ricardocolladorothschild@gmail.com>
2024-02-03 21:54:02 -0400

    Modified packagejson configure for deploy

--- api/routes/users.routes.ts
+++ api/routes/users.routes.ts
@@ -10,7 +10,7 @@ router.get('/', async (req: Request, res: Response)=>{
     try{
 
         
-        console.log('showing data....');
+        
         
             res
                 .status(200)
@@ -27,13 +27,13 @@ router.get('/all', async (req: Request, res: Response)=>{
         try{
 
             const user = await Users.findAll();
-            console.log(user);
+            
             
                 res
                     .status(200)
                     .json({user});
         }catch(error){
-            console.log(error)
+            
             res
             .status(500)
             .json({error:`Internal server error - 500: ${error}`});
